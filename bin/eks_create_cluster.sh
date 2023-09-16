ba310a011438d9de2e0dbd47232faf8fd0e6ed30
LinhCN <nguyenlinhtt2cbl@gmail.com>
2023-09-16 18:55:21 +0700

    submit

--- bin/eks_create_cluster.sh
+++ bin/eks_create_cluster.sh
@@ -7,5 +7,8 @@ if kubectl config get-clusters | grep -q myCluster; then
 else
     echo
     echo "Creating cluster..."
-    eksctl create cluster --config-file=./infrastructure/infra.yml --timeout=60m --profile=eks
+    # eksctl create cluster --config-file=./infrastructure/infra.yml --timeout=60m --profile=eks
+    aws eks create-cluster --region us-est-1 --name myCluster --kubernetes-version 1.27 \
+        --role-arn arn:aws:iam::263031845436:user/linhcn3:role/eksClusterRole \
+        --resources-vpc-config subnetIds=eks-cluster-sg-myCluster-775609682,eks-cluster-sg-myCluster-775609682,securityGroupIds=eks-cluster-sg-myCluster-775609682
 fi
